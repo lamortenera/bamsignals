840ae1846493060ab758820d3f3cd155f107d989
Johannes Helmuth <helmuth@molgen.mpg.de>
2015-01-29 17:58:03 +0100

    Moved artificial read generation to seperate files. Test have order numbers now

--- tests/testthat/test_1count.R
+++ tests/testthat/test_1count.R
@@ -1,50 +1,10 @@
-# testthat script for bamsignals::count
+# testthat script for bamsignals::count()
 #
 # 2015-01-28
 
 library(bamsignals)
 context("bamsignals::count()")
 
-
-#
-# Artificial data preparation
-#
-binwidth <- 500
-regionlength <- 10*binwidth
-
-#1. generate list of reads and sort by starting position
-readpos <- matrix( rnbinom( 2 * regionlength, 50, .99), nrow=2 )
-reads <- list()
-for (i in 1:ncol(readpos)) {
-	if (readpos[1,i]!=0) {
-		for (j in 1:readpos[1,i]) {
-			reads <- append(reads, list(c(i, min(i+50-1, regionlength), 0, floor(runif(1, 0, 50)))))
-		}
-	}
-	if (readpos[2,i]!=0) {
-		for (j in 1:readpos[2,i]) {
-			reads <- append(reads, list(c(max(i-50+1,0), i, 16, floor(runif(1, 0, 50)))))
-		}
-	}
-}
-out <- do.call("rbind", reads)
-out <- out[ order(out[,1]),]
-
-#2. write to SAM file
-dir.create("data", showWarnings=F)
-sam.file.se <- "data/bamsignals_SE_counts.sam"
-writeLines("@HD\tVN:1.0\tSO:unsorted\n@SQ\tSN:chr1\tLN:249250621", sam.file.se)
-write.table(x=cbind("*", out[,3], "chr1", out[,1], out[,4], "50M", "*", "0", "0", "*", "*"),
-			file=sam.file.se,
-			sep="\t",
-			quote=F,
-			row.names=F,
-			col.names=F,
-			append=T
-			)
-
-#3. SAM -> BAM, BAM->BAMi
-bamsignals:::writeSamAsBamAndIndex(sam.file.se, "data/bamsignals_SE_counts.bam")
 
 #
 # Test cases
