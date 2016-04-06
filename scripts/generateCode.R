d3340ad1e72debde6e475d800e7d019424cfc02b
Alessandro Mammana <mammana@molgen.mpg.de>
2016-04-06 21:59:41 +0000

    put correct version number and fixed generator script
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bamsignals@115926 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/generateCode.R
+++ scripts/generateCode.R
@@ -7,16 +7,35 @@
 #R --slave < bamsignals/scripts/generateCode.R
 #after that you can use R CMD build and install the tar.gz
 
+load_or_get <- function(pkg){
+	if (!require(pkg, character.only=T)) install.packages(pkg, dependencies=TRUE)
+	library(pkg, character.only=T)
+}
+
 #wrap C++ functions into R functions
 pckg <- "bamsignals"
-library(Rcpp)
+load_or_get("Rcpp")
 compileAttributes(pckg)
 
 #create man pages and the NAMESPACE file
-library(roxygen2)
+load_or_get("devtools")
+devtools::install_deps(pckg, dependencies=TRUE)
+load_or_get("roxygen2")
 roxygenize(pckg)
 
-#register function (needs the Kmisc package from CRAN)
+# automatically register functions. Needs the Kmisc package
+# from github, NOT from CRAN (it's behind of many commits).
+# To install it, do:
+# devtools::install_github("kevinushey/Kmisc")
+# the last commit that was working was:
+# x=b9a340ecef5bdd61fb09f462c179ccb86aff4e31, so in case
+# the newest version doesn't work, a quick fix would be
+# devtools::install_github("kevinushey/Kmisc", rev=x)
+if (!require("Kmisc")){
+	load_or_get("devtools")
+	devtools::install_github("kevinushey/Kmisc")
+}
+
 setwd(pckg)
 Kmisc:::registerFunctions("")
 setwd("../")
